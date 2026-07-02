aa0214272d910c8fe90dcf90d267738b5a20e355
Utazi Fredrick <utazifredrick530@gmail.com>
2026-07-02 15:58:50 +0100

    Initial Daily Schedule App setup
    Add basic project structure with HTML entry point, main JavaScript file with date-based logic, and schedule database module containing day-of-week data structure.

---
+++ index.js
@@ -0,0 +1,29 @@
+import { dailyScheduleDataBase } from "./scheduleDataBase.js";
+
+const date = new Date().getDate();
+
+switch (date) {
+  case 0:
+    alert("");
+    break;
+  case 1:
+    alert("");
+    break;
+  case 2:
+    alert("");
+    break;
+  case 3:
+    alert("");
+    break;
+  case 4:
+    alert("");
+    break;
+  case 5:
+    alert("");
+    break;
+  case 6:
+    alert("");
+    break;
+  default:
+    alert("");
+}
